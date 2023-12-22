e3d984dc576259d62ad971a7637ec9f1e9ba091a
HanSeongLee <jkhanseong@gmail.com>
2023-12-22 18:14:41 +0900

    feat: add displaying compress info

--- src/index.ts
+++ src/index.ts
@@ -29,30 +29,56 @@ if (!fs.existsSync(outputDir)) {
     process.exit(1);
 }
 
-function convertFile(inputFile: string, outputFile: string): void {
-    console.log(`Converting file "${inputFile}" to "${outputFile}"...`);
+function formatBytes(bytes: number, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+function printHeader() {
+    console.log('File Path'.padEnd(50) + 'Original Size'.padEnd(20) + 'Compressed Size'.padEnd(20) + 'Compression Ratio');
+    console.log('-'.repeat(110));
+}
+
+function convertFile(inputFile: string, outputFile: string, relativePath: string): void {
+    const originalSize = fs.statSync(inputFile).size;
 
     sharp(inputFile)
         .webp({ quality: quality })
-        .toFile(outputFile, (err) => {
-            if (err) {
-                console.error(`Failed to convert file "${inputFile}" to "${outputFile}".`, err);
-                process.exit(1);
-            }
+        .toBuffer()
+        .then(outputBuffer => {
+            fs.writeFileSync(outputFile, outputBuffer);
+            const compressedSize = outputBuffer.length;
+            const compressionRatio = ((originalSize - compressedSize) / originalSize * 100).toFixed(2);
+
+            console.log(
+                `${relativePath.padEnd(50)}\t${formatBytes(originalSize).padEnd(20)}\t${formatBytes(compressedSize).padEnd(20)}\t${compressionRatio}%`
+            );
+        })
+        .catch(err => {
+            console.error(`Failed to convert file "${inputFile}" to "${outputFile}".`, err);
+            process.exit(1);
         });
 }
 
-function convertDirectory(inputDir: string, outputDir: string): void {
+function convertDirectory(inputDir: string, outputDir: string, baseDir: string = inputDir): void {
     const files = fs.readdirSync(inputDir);
 
     files.forEach((file) => {
         const inputFile = path.join(inputDir, file);
+        const relativePath = path.relative(baseDir, inputFile);
 
         if (fs.statSync(inputFile).isDirectory()) {
             const subInputDir = inputFile;
             const subOutputDir = path.join(outputDir, file);
             fs.ensureDirSync(subOutputDir);
-            convertDirectory(subInputDir, subOutputDir);
+            convertDirectory(subInputDir, subOutputDir, baseDir);
         } else {
             const outputFile = path.join(outputDir, `${path.parse(file).name}.webp`);
 
@@ -62,9 +88,10 @@ function convertDirectory(inputDir: string, outputDir: string): void {
                 return;
             }
 
-            convertFile(inputFile, outputFile);
+            convertFile(inputFile, outputFile, relativePath);
         }
     });
 }
 
+printHeader();
 convertDirectory(inputDir, outputDir);
